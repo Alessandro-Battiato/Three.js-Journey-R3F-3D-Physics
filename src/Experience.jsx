962a3d949d7b09faf12ab0947e462c29892e6605
alessandrobattiato <alessandroDev@outlook.it>
2024-08-24 16:26:47 +0200

    feat: add torus geometry

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -15,19 +15,19 @@ export default function Experience() {
             <Physics debug>
                 {/*Physics aren't enabled by default using the Physics tag, you need to use the RigidBody and items will fall*/}
                 <RigidBody colliders="ball">
-                    <mesh castShadow position={[-2, 2, 0]}>
+                    <mesh castShadow position={[0, 4, 0]}>
                         <sphereGeometry />
                         <meshStandardMaterial color="orange" />
                     </mesh>
                 </RigidBody>
 
                 <RigidBody>
-                    <mesh castShadow position={[2, 2, 0]}>
-                        <boxGeometry args={[3, 2, 1]} />
-                        <meshStandardMaterial color="mediumpurple" />
-                    </mesh>
-                    <mesh castShadow position={[2, 2, 3]}>
-                        <boxGeometry args={[1, 1, 1]} />
+                    <mesh
+                        castShadow
+                        position={[0, 1, 0]}
+                        rotation={[Math.PI * 0.1, 0, 0]}
+                    >
+                        <torusGeometry args={[1, 0.5, 16, 32]} />
                         <meshStandardMaterial color="mediumpurple" />
                     </mesh>
                 </RigidBody>
